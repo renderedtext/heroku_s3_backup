e43477e64f438d3fc4f49fcabe3db8d9a42700a9
Patrick Crowley <patrick@mokolabs.com>
2011-02-01 08:44:53 -0800

    added support for setting custom S3 bucket, path, and filenames

--- lib/heroku_s3_backup.rb
+++ lib/heroku_s3_backup.rb
@@ -1,10 +1,36 @@
 require 'fog'
 
 class HerokuS3Backup
-  def self.backup
+  def self.backup(options = {})
     begin
       puts "[#{Time.now}] heroku:backup started"
-      name = "#{ENV['APP_NAME']}-#{Time.now.strftime('%Y-%m-%d-%H%M%S')}.sql"
+      
+      # Set app
+      app = if options[:app]
+        if options[:name] == false
+          ""
+        else
+          options[:name] + "-"
+        end
+      else
+        ENV['APP_NAME'] + "-"
+      end
+      
+      # Set bucket
+      bucket = if options[:bucket]
+        options[:bucket]
+      else
+        "#{app}-heroku-backups"
+      end
+      
+      # Set path
+      path = if options[:path]
+        options[:path]
+      else
+        "db"
+      end
+      
+      name = "#{app}#{Time.now.strftime('%Y-%m-%d-%H%M%S')}.sql"
 
       db = ENV['DATABASE_URL'].match(/postgres:\/\/([^:]+):([^@]+)@([^\/]+)\/(.+)/)
       system "PGPASSWORD=#{db[2]} pg_dump -Fc -i --username=#{db[1]} --host=#{db[3]} #{db[4]} > tmp/#{name}"
@@ -14,12 +40,6 @@ class HerokuS3Backup
 
       backup_path = "tmp/#{name}.gz"
 
-      bucket_name = if ENV['backup_bucket']
-        ENV['backup_bucket']
-      else
-        "#{ENV['APP_NAME']}-heroku-backups"
-      end
-
       s3 = Fog::Storage.new(
         :provider => 'AWS',
         :aws_access_key_id => ENV['S3_KEY'],
@@ -28,14 +48,14 @@ class HerokuS3Backup
       s3.get_service
     
       begin
-        s3.get_bucket(bucket_name)
-        directory = s3.directories.get(bucket_name)
+        s3.get_bucket(bucket)
+        directory = s3.directories.get(bucket)
       rescue Excon::Errors::NotFound
-        directory = s3.directories.create(:key => bucket_name)
-        s3.get_bucket(bucket_name)
+        directory = s3.directories.create(:key => bucket)
+        s3.get_bucket(bucket)
       end
 
-      directory.files.create(:key => "db/#{name}.gz", :body => open(backup_path))
+      directory.files.create(:key => "#{path}/#{name}.gz", :body => open(backup_path))
       system "rm #{backup_path}"
       puts "[#{Time.now}] heroku:backup complete"
       

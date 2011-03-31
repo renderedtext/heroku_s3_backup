e3f3e61cc95a5291fcb4521ea96b5878276b17a2
Darko Fabijan <darko@renderedtext.com>
2011-03-31 21:07:54 +0200

    Changing env vars.

--- lib/heroku_s3_backup.rb
+++ lib/heroku_s3_backup.rb
@@ -4,7 +4,7 @@ class HerokuS3Backup
   def self.backup(options = {})
     begin
       puts "[#{Time.now}] heroku:backup started"
-      
+
       # Set app
       app = if options[:name] == false
       elsif options[:name]
@@ -12,28 +12,28 @@ class HerokuS3Backup
       else
         ENV['APP_NAME'] + "-"
       end
-      
+
       # Set bucket
       bucket = if options[:bucket]
         options[:bucket]
       else
         "#{app}-heroku-backups"
       end
-      
+
       # Set path
       path = if options[:path]
         options[:path]
       else
         "db"
       end
-      
+
       # Set timestamp
       timestamp = if options[:timestamp]
         options[:timestamp]
       else
         "%Y-%m-%d-%H%M%S"
       end
-      
+
       name = "#{app}#{Time.now.strftime(timestamp)}.sql"
 
       db = ENV['DATABASE_URL'].match(/postgres:\/\/([^:]+):([^@]+)@([^\/]+)\/(.+)/)
@@ -46,11 +46,11 @@ class HerokuS3Backup
 
       s3 = Fog::Storage.new(
         :provider => 'AWS',
-        :aws_access_key_id => ENV['S3_KEY'],
-        :aws_secret_access_key => ENV['S3_SECRET']
+        :aws_access_key_id => ENV['S3_ACCESS_KEY_ID'],
+        :aws_secret_access_key => ENV['S3_SECRET_ACCESS_KEY']
       )
       s3.get_service
-    
+
       begin
         s3.get_bucket(bucket)
         directory = s3.directories.get(bucket)
@@ -62,7 +62,7 @@ class HerokuS3Backup
       directory.files.create(:key => "#{path}/#{name}.gz", :body => open(backup_path))
       system "rm #{backup_path}"
       puts "[#{Time.now}] heroku:backup complete"
-      
+
     rescue Exception => e
       if ENV['HOPTOAD_KEY']
         require 'toadhopper'
